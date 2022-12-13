1d2d94f94ff75945e85239fbad2cefed24ff3fc5
Prasath learning <prasath.learning@gmail.com>
2022-12-13 10:02:19 +0000

    Deleted gatsby-node.js

--- gatsby-node.js
+++
@@ -1,5 +0,0 @@
-exports.createPages = require("./gatsby/node/createPages");
-exports.createResolvers = require("./gatsby/node/createResolvers");
-exports.onCreateNode = require("./gatsby/node/onCreateNode");
-exports.onCreateWebpackConfig = require("./gatsby/node/onCreateWebpackConfig");
-exports.sourceNodes = require("./gatsby/node/sourceNodes");
